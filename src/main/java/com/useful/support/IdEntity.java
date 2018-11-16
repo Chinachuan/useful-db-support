18363af12d85b612a7bb75f0ce3720d7b3fe9633
Chinachuan <602030912@qq.com>
2018-11-16 19:09:15 +0800

    数据库中心

--- src/main/java/com/useful/support/IdEntity.java
+++ src/main/java/com/useful/support/IdEntity.java
@@ -18,28 +18,12 @@ import javax.persistence.Version;
 @MappedSuperclass
 public class IdEntity implements Serializable{
     private static final long serialVersionUID = 1L;
-	public static int STATUS_CANCEL = 3;//已取消
-    public static int STATUS_INIT = 2;//处理中
-	public static int STATUS_SUCC = 1;//成功
-	public static int STATUS_FAIL = 0;//失败
-	public static int STATUS_NONE = 9;//未知
-	public static int STATUS_REFUND = 4;//退款
-	
-	public static int PHASE_NONE = 9;//未知
-	public static int PHASE_INIT= 0;//处理中
-	public static int PHASE_RECORDED= 1;//已记账
-	public static int PHASE_CHECKED = 2;//已对账
-	public static int PHASE_COMFIRM= 8;//确认成功
-	
-	public static int SETTLE_NOT = -1;//非秒到交易订单
-	public static int SETTLE_SUCC = 1;//已经发起结算
-	public static int SETTLE_INIT = 2;//未发起结算
-
 	public static int AUTO_ID = 1000;//起始ID
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     @Column(length=20)
     protected Long id;
+    
     @Version
     private Integer optimistic;
 
